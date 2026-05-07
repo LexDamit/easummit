905de668c42a1c53a9f5db46bcb4515ae176b77d
Lex Damit <lex.damit@gmail.com>
2026-05-07 18:09:37 +0200

    Harden Firebase private key parsing

--- netlify/functions/get-registration.js
+++ netlify/functions/get-registration.js
@@ -1,6 +1,18 @@
 const { cert, getApps, initializeApp } = require('firebase-admin/app')
 const { getFirestore } = require('firebase-admin/firestore')
 
+const normalizePrivateKey = (value) => {
+  if (!value) {
+    return value
+  }
+
+  return String(value)
+    .trim()
+    .replace(/^"(.*)"$/s, '$1')
+    .replace(/^'(.*)'$/s, '$1')
+    .replace(/\\n/g, '\n')
+}
+
 const getAdminDb = () => {
   if (
     !process.env.FIREBASE_PROJECT_ID ||
@@ -15,7 +27,7 @@ const getAdminDb = () => {
       credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        privateKey: normalizePrivateKey(process.env.FIREBASE_PRIVATE_KEY),
       }),
     })
   }
